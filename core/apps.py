934b221a9550362e83e23ff58ac0b27c34ca7444
Srishti-Doshi <srishtidoshi7@gmail.com>
2025-12-23 23:16:31 +0530

    fix issue

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,21 @@
 from django.apps import AppConfig
-
+import os
 
 class CoreConfig(AppConfig):
-    name = 'core'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "core"
+
+    def ready(self):
+        if os.environ.get("CREATE_SUPERUSER") == "1":
+            try:
+                from django.contrib.auth.models import User
+
+                if not User.objects.filter(username="admin").exists():
+                    User.objects.create_superuser(
+                        username="admin",
+                        email="admin@example.com",
+                        password="admin123"
+                    )
+                    print("✅ Superuser created")
+            except Exception as e:
+                print("❌ Superuser creation error:", e)
