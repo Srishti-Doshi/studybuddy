1025628dcd07ef4a54a5f069742076339af48beb
Srishti-Doshi <srishtidoshi7@gmail.com>
2025-12-23 22:48:13 +0530

    temporary change

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -175,4 +175,19 @@ if DEBUG:
         pass
 
 
+import os
+
+if os.environ.get("CREATE_SUPERUSER") == "1":
+    try:
+        from django.contrib.auth.models import User
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@example.com",
+                password="admin123"
+            )
+            print("✅ Superuser created")
+    except Exception as e:
+        print("❌ Superuser creation failed:", e)
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
